405fc88114a22f34c7f1ac744f3c7b590a137852
Emmanuel Belini <beliniemmanuel@gmail.com>
2024-03-12 00:56:29 -0300

    obrigado livro de 5mil anos atras

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,16 @@
-num = int(input("Escreva seu numero: "))
+def fibonacci(n):
+    sequencia = [0, 1]
+    while sequencia[-1] < n:
+        sequencia.append(sequencia[-1] + sequencia[-2])
+    return sequencia
 
-for i in range(0,31):
-    print(num + num - 1)
+def pertence(number):
+    sequence = fibonacci(number)
+    if number in sequence:
+        return f"O número {number} pertence à sequência de Fibonacci."
+    else:
+        return f"O número {number} não pertence à sequência de Fibonacci."
+
+
+numero = int(input("Digite um número para verificar se pertence à sequência de Fibonacci: "))
+print(pertence(numero))
